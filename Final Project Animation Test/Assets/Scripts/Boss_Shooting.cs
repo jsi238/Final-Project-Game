59572924291c7417c0de63dcd4b1fe71647da0b8
Primary <yw3726@nyu.com>
2019-05-06 11:36:17 -0400

    Leo's project replacing everything (only animations and sprites are included)

---
+++ Final Project Animation Test/Assets/Scripts/Boss_Shooting.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform ThisTransform;
+    Vector3 GameobjectCurrentPosition;
+    public float ShotAngle;
+    public GameObject LeftWing;
+    public GameObject RightWing;
+
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Tripleshots();
+        }
+
+        /*
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            Fiveshots();
+            timer = 0;
+        }
+        */
+    }
+
+    void Tripleshots()
+    {
+        ThisTransform = this.transform;
+        ShotAngle = ThisTransform.rotation.z;
+        Instantiate(bullet, ThisTransform.position, new Quaternion(0, 0, ShotAngle, 1));
+        Instantiate(bullet, ThisTransform.position, new Quaternion(0, 0, ShotAngle - 0.05f, 1));
+        Instantiate(bullet, ThisTransform.position, new Quaternion(0, 0, ShotAngle + 0.05f, 1));
+    }
+
+    void Finalshots()
+    {
+        //Where bugs are: Bu
+        ThisTransform = this.transform;
+        for (int asd = 0; asd < 7; asd++)
+        {
+            ShotAngle = ThisTransform.rotation.z;
+            Instantiate(bullet, ThisTransform.position, new Quaternion(0, 0, Random.Range(ShotAngle - 0.07f, ShotAngle + 0.07f), 1),this.transform);
+
+            ShotAngle = ThisTransform.rotation.z + 0.1f;
+            Instantiate(bullet, LeftWing.transform.position, new Quaternion(0, 0, Random.Range(ShotAngle - 0.07f, ShotAngle + 0.07f), 1),this.transform);
+
+            ShotAngle = ThisTransform.rotation.z - 0.1f;
+            Instantiate(bullet, RightWing.transform.position, new Quaternion(0, 0, Random.Range(ShotAngle - 0.07f, ShotAngle + 0.07f), 1),this.transform);
+        }
+    }
+
+    void ShootABullet(Vector3 spawnpose,float direction)
+    {
+        
+    }
+}
---
+++ Final Project Animation Test/Assets/Scripts/bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public Rigidbody2D me;
+    public float speed;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Sprite sprite3;
+    public SpriteRenderer mySR;
+
+    void Start()
+    {
+        me = GetComponent<Rigidbody2D>();
+        mySR = GetComponent<SpriteRenderer>();
+        speed = 150;
+    }
+    
+    void Update()
+    {
+        me.velocity = transform.up * speed * Time.deltaTime;
+        speed = speed * (4 * Time.deltaTime+1);
+        if (speed > 1000)
+        {
+            mySR.sprite = sprite3;
+        }
+        else if (speed > 500)
+        {
+            mySR.sprite = sprite2;
+        }
+
+        if (speed > 5000)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
